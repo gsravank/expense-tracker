ddeaef482953cdabdffac502c05897b583c025e3
G Sravan Kumar <sravan335@gmail.com>
2017-11-21 13:13:34 +0530

    No need for source information in transaction and rewrite method to get transactions

--- expense_tracker/transaction.py
+++ expense_tracker/transaction.py
@@ -1,37 +1,37 @@
+from imessage import get_recent_messages
+
+
 class Transaction:
-        def __init__(self, message):
-            self.message = message
-            self.source = ''
-            self.amount = 0
-            self.vendor_name = ''
+    def __init__(self, message):
+        self.message = message
+        self.amount = 0
+        self.vendor_name = ''
 
-            # Evaluate source, vendor names and the amount for the transaction from message details
-            self.get_source()
-            self.get_amount()
-            self.get_vendor()
+        # Extract vendor name and the amount for the transaction from message text
+        self.get_amount()
+        self.get_vendor()
 
-        def get_source(self):
-            name_or_number = self.message.user.name_or_number
+    def get_amount(self):
+        self.amount = 0
 
-            if 'paytm' in name_or_number:
-                self.source = 'Paytm'
-            elif 'citi' in  name_or_number:
-                if 'debit card' in self.message.text.lower():
-                    self.source = 'Citi Debit'
-                elif 'credit card' in self.message.text.lower():
-                    self.source = 'Citi Credit'
-            else:
-                self.source = 'Unknown'
+    def get_vendor(self):
+        self.vendor_name = ''
 
-        def get_amount(self):
-            self.amount = 0
+    def __repr__(self):
+        return 'Amount: {}, Via: {}, To: {}'.format(self.amount, self.source, self.vendor_name)
 
-        def get_vendor(self):
-            self.vendor_name = ''
 
-        def __repr__(self):
-            return 'Amount: {}, Via: {}, To: {}'.format(self.amount, self.source, self.vendor_name)
+def check_valid_transaction():
+    return
 
 
 def get_recent_transactions(period='1w'):
-    return
+    recent_messages = get_recent_messages(period=period)
+
+    transactions = list()
+
+    for message in recent_messages:
+        if check_valid_transaction(message):
+            transactions.append(Transaction(message))
+        
+    return transactions
