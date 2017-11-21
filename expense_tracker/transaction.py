6e110e3f92db7d5901978e3dc436681df084aea2
G Sravan Kumar <sravan335@gmail.com>
2017-11-21 13:43:26 +0530

    Use a json file to store all expense patterns

--- expense_tracker/transaction.py
+++ expense_tracker/transaction.py
@@ -1,4 +1,5 @@
 from imessage import get_recent_messages
+import json
 
 
 class Transaction:
@@ -21,17 +22,20 @@ class Transaction:
         return 'Amount: {}, Via: {}, To: {}'.format(self.amount, self.source, self.vendor_name)
 
 
-def check_valid_transaction():
+def check_valid_transaction(message_content, expense_patterns):
     return
 
 
 def get_recent_transactions(period='1w'):
     recent_messages = get_recent_messages(period=period)
 
+    with open('data/expense_patterns.json', 'rb') as f:
+        expense_patterns = json.load(f)
+
     transactions = list()
 
     for message in recent_messages:
-        if check_valid_transaction(message):
-            transactions.append(Transaction(message))
-        
+        if check_valid_transaction(message.text, expense_patterns):
+            transactions.append(message)
+
     return transactions
